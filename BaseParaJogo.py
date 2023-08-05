890c9b6a4395bdb3879d518245a8dcb6dc099f1f
Oberlan Romão <oberlan@gmail.com>
2023-08-05 14:24:24 -0300

    Inclusão das funções 'posicaoCursorMouse' e 'botaoMousePressionado'

--- BaseParaJogo.py
+++ BaseParaJogo.py
@@ -1,6 +1,7 @@
 
 import pygame
 from pygame.locals import *
+from typing import Tuple
 import os
 
 __tela = None
@@ -51,7 +52,7 @@ def finalizaJogo():
 def tempoExecutandoJogo() -> int:
     return pygame.time.get_ticks() - __tempoInicio
 
-def atualizaTelaJogo():
+def atualizaTelaJogo() -> None:
     global __clock
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -60,25 +61,27 @@ def atualizaTelaJogo():
     pygame.display.update()
     __clock.tick(60)  # limits FPS to 60
 
-def limpaTela():
+def limpaTela() -> None:
     atualizaCorFundo(__corFundo)
-   
-def atualizaCorFundo(cor: pygame.Color):
+
+def atualizaCorFundo(cor: pygame.Color) -> None:
     global __corFundo
     __corFundo = cor
     __tela.fill(cor)
 
 
-def desenhaTexto(mensagem: str, x: int, y: int, tamFonte: int, cor: pygame.Color, nomeFonte: str = None):
+def desenhaTexto(mensagem: str, x: int, y: int, tamFonte: int, cor: pygame.Color = "black", nomeFonte: str = None) -> None:
     if nomeFonte:
         nomeFonte = f"{os.getcwd()}/{nomeFonte}"
     fonte = pygame.font.Font(nomeFonte, tamFonte)
     texto = fonte.render(mensagem, True, cor)
-    textoPos = texto.get_rect(x=x, y=y)
+    textoPos = texto.get_rect()
+    textoPos.centerx = x
+    textoPos.centery = y
     __tela.blit(texto, textoPos)
 
 
-def desenhaRetangulo(x: int, y: int, largura: int, altura: int, corFundo: pygame.Color):
+def desenhaRetangulo(x: int, y: int, largura: int, altura: int, corFundo: pygame.Color) -> None:
     pygame.draw.rect(__tela, corFundo, (x, y, largura, altura))
 
 
@@ -112,7 +115,7 @@ def desenhaFigura(numFigura: int, x: int, y: int) -> None:
 def teclaPressionada(tecla: int) -> bool:
     return pygame.key.get_pressed()[tecla]
 
-def teclaLiberada(tecla):
+def teclaLiberada(tecla) -> bool:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             finalizaJogo()
@@ -120,7 +123,19 @@ def teclaLiberada(tecla):
             return True
     return False
 
-def carregaSom(nomeArquivo: str):
+def posicaoCursorMouse() -> Tuple[int, int]:
+    return pygame.mouse.get_pos()
+
+
+def botaoMousePressionado() -> Tuple[bool, int, Tuple[int, int]]:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            finalizaJogo()
+        if evento.type == MOUSEBUTTONDOWN:
+            return True, evento.button, evento.pos
+    return False, -1, (-1, -1)
+
+def carregaSom(nomeArquivo: str) -> int:
     global __numSomCarregado
     nomeArquivoCompleto = f"{os.getcwd()}/{nomeArquivo}"
     try:
@@ -133,13 +148,13 @@ def carregaSom(nomeArquivo: str):
         raise SystemExit
     return __numSomCarregado
 
-def tocaSom(numSom: int):
+def tocaSom(numSom: int) -> None:
     if numSom <= 0 or numSom > __numSomCarregado:
         print("ERRO - Número do som inválido!")
         return
     __listaSons[numSom - 1].play()
 
-def carregaMusica(nomeArquivo: str):
+def carregaMusica(nomeArquivo: str) -> int:
     global __numMusicaCarregada
     nomeArquivoCompleto = f"{os.getcwd()}/{nomeArquivo}"
     if os.path.isfile(nomeArquivoCompleto):
@@ -152,7 +167,7 @@ def carregaMusica(nomeArquivo: str):
         raise SystemExit
     return __numMusicaCarregada
     
-def iniciaMusica(numMusica: int, loop: bool = True):
+def iniciaMusica(numMusica: int, loop: bool = True) -> None:
     if numMusica <= 0 or numMusica > __numMusicaCarregada:
         print("ERRO - Número da música invalido!")
         return
@@ -162,7 +177,7 @@ def iniciaMusica(numMusica: int, loop: bool = True):
     else: 
         pygame.mixer.music.play()
 
-def paraMusica():
+def paraMusica() -> None:
     pygame.mixer.music.stop()
 
 
